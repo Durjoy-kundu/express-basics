965b42a4b146fadfc2358a8ee66efbd928d29037
Durjoy kundu <durjoykundu07@gmail.com>
2025-01-30 11:59:50 +0600

    request params,queries and static files

---
+++ agenda.md
@@ -0,0 +1,9 @@
+
+            ...Agenda ...
+
+
+1.why should use express
+2. why nodemon?
+3. installing express
+4. request parameters and queries
+5. Static files
--- basic.js
+++ basic.js
@@ -2,11 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+// app.get, app.post , app.put, app.delete
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World2!')
 })
+
+app.get('/about', (req, res) => {
+  res.send('Hello about!')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('Hello contact!')
+})
+
+
+app.get('/blog', (req, res) => {
+  res.send('Hello blogs!')
+})
+// for this
+//  http://localhost:3000/blog/intro-to-sabuj?mode=dark&region=in
+app.get('/blog/:slug', (req, res) => {
+  console.log(req.params) // { slug: 'intro-to-sabuj' }
+  console.log(req.query) // { mode: 'dark', region: 'in' }
+  res.send(`hello ${req.params.slug}`)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
- 
